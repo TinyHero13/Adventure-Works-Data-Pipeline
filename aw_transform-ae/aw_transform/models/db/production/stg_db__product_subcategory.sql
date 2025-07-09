4153ac0eeb610e7f057362a8519c49237aaf78b1
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-09 13:38:30 -0300

    feat: add stg_db_product_subcategory model

---
+++ aw_transform-ae/aw_transform/models/db/production/stg_db__product_subcategory.sql
@@ -0,0 +1,10 @@
+with product_subcategory as (
+    select
+        cast(productsubcategoryid as int) as product_subcategory_id
+        , cast(productcategoryid as int) as product_category_id
+        , name
+    from {{ source('source_db', 'product_subcategory') }}
+)
+
+select *
+from product_subcategory

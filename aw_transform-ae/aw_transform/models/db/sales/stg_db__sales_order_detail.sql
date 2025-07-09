5565b45166c8d1c918b5a1a9998700fdc34779c4
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-09 13:44:47 -0300

    feat: add stg_db_sales_order_detail model

---
+++ aw_transform-ae/aw_transform/models/db/sales/stg_db__sales_order_detail.sql
@@ -0,0 +1,15 @@
+with sales_order_detail as (
+    select
+        cast(salesorderid as int) as sales_order_id
+        , cast(salesorderdetailid as int) as sales_order_detail_id
+        , carriertrackingnumber as carrier_tracking_number
+        , cast(productid as int) as product_id
+        , cast(orderqty as int) as order_quantity
+        , cast(unitprice as numeric(19,4)) as unit_price
+        , cast(unitpricediscount as numeric(19,4)) as unit_price_discount
+        , cast(linetotal as numeric(38,6)) as line_total
+    from {{ source('source_db', 'sales_order_detail') }}
+)
+
+select *
+from sales_order_detail

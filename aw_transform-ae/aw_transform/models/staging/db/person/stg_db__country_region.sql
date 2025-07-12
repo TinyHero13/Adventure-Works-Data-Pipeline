2a421090b928de31960adf87aa006d1a089b6456
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-12 12:05:31 -0300

    feat: add stg_db_country_region model

---
+++ aw_transform-ae/aw_transform/models/staging/db/person/stg_db__country_region.sql
@@ -0,0 +1,9 @@
+with country_region as (
+    select
+        countryregioncode as country_region_code
+        , name as country_region_name
+    from {{ source('source_db', 'country_region') }}
+)
+
+select *
+from country_region

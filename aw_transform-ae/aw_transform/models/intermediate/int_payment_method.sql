97eb0a203ca4d5d9c8ddf8805ce18cc0c28e6b21
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-14 18:05:13 -0300

    feat:add int_payment_methodn model

---
+++ aw_transform-ae/aw_transform/models/intermediate/int_payment_method.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='ephemeral') }}
+
+with credit_cards as (
+    select * from {{ ref('stg_db__credit_card') }}
+),
+
+payment_types as (
+    select distinct
+        card_type as payment_method_name
+    from credit_cards
+),
+
+dim_payment_method as (
+    select 
+        row_number() over (order by payment_method_name) as payment_method_id
+        , payment_method_name
+    from payment_types
+)
+
+select * 
+from dim_payment_method

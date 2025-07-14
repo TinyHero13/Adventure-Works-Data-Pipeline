6a4a2e23c91486dde8a27b48f88a4e413c8a48a5
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-14 18:16:09 -0300

    refactor: adjusting sql for best practices

--- aw_transform-ae/aw_transform/models/intermediate/int_payment_method.sql
+++ aw_transform-ae/aw_transform/models/intermediate/int_payment_method.sql
@@ -7,16 +7,16 @@ with
 )
 
     , payment_types as (
-    select distinct
-        card_type as payment_method_name
-    from credit_cards
+        select distinct
+            card_type as payment_method_name
+        from credit_cards
     )
 
     , dim_payment_method as (
-    select 
-        row_number() over (order by payment_method_name) as payment_method_id
-        , payment_method_name
-    from payment_types
+        select 
+            row_number() over (order by payment_method_name) as payment_method_id
+            , payment_method_name
+        from payment_types
     )
 
 select * 

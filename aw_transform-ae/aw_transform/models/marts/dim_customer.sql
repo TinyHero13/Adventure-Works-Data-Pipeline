0dbb237c358dd171c756ab7b13ff1cab3d0ce508
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-15 13:24:26 -0300

    feat: add schema for all marts files

--- aw_transform-ae/aw_transform/models/marts/dim_customer.sql
+++ aw_transform-ae/aw_transform/models/marts/dim_customer.sql
@@ -1,6 +1,8 @@
 with 
     dim_customers as (
-        select * 
+        select 
+            id_cliente as customer_id
+            , full_name
         from {{ ref('int_sales_customer_persons') }}
     )
 

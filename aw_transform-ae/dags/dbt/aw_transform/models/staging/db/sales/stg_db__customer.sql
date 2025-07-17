c989e9b1363338fb8a4071872ee2cc12467a77a6
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-17 15:47:39 -0300

    refactor: add timestamp and renamed pk and fk to better visualizing for sales

--- aw_transform-ae/dags/dbt/aw_transform/models/staging/db/sales/stg_db__customer.sql
+++ aw_transform-ae/dags/dbt/aw_transform/models/staging/db/sales/stg_db__customer.sql
@@ -1,7 +1,11 @@
 with customer as (
     select
-        cast(customerid as int) as customer_id
-        , cast(personid as int) as person_id
+        cast(customerid as int) as customer_pk
+        , cast(personid as int) as person_fk
+        , cast(storeid as int) as store_fk
+        , cast(territoryid as int) as territory_fk
+        , accountnumber as account_number
+        , current_timestamp() as updated_at
     from {{ source('source_db', 'customer') }}
 )
 
--- aw_transform-ae/dags/dbt/aw_transform/models/staging/db/sales/stg_db__sales_territory.sql
+++ aw_transform-ae/dags/dbt/aw_transform/models/staging/db/sales/stg_db__sales_territory.sql
@@ -1,10 +1,14 @@
 with sales_territory as (
     select
-        cast(territoryid as int) as territory_id
+        cast(territoryid as int) as territory_pk
         , name as territory_name
         , countryregioncode as country_region_code
+        , group
         , salesytd as sales_ytd
         , saleslastyear as sales_last_year
+        , costytd as cost_ytd
+        , costlastyear as cost_last_year
+        , current_timestamp() as updated_at
     from {{ source('source_db', 'sales_territory') }}
 )
 

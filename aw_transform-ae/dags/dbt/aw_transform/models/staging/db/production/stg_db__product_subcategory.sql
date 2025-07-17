1a1d4b716f0095e3f1a801e7c6b428e3e9110117
YasmimAbrahao <yasmim.lima@indicium.tech>
2025-07-17 16:10:39 -0300

    refactor: add timestamp and renamed pk and fk to better visualizing for product

--- aw_transform-ae/dags/dbt/aw_transform/models/staging/db/production/stg_db__product_subcategory.sql
+++ aw_transform-ae/dags/dbt/aw_transform/models/staging/db/production/stg_db__product_subcategory.sql
@@ -1,8 +1,9 @@
 with product_subcategory as (
     select
-        cast(productsubcategoryid as int) as product_subcategory_id
-        , cast(productcategoryid as int) as product_category_id
+        cast(productsubcategoryid as int) as product_subcategory_pk
+        , cast(productcategoryid as int) as product_category_fk
         , name as product_subcategory_name
+        , current_timestamp() as updated_at
     from {{ source('source_db', 'product_subcategory') }}
 )
 
